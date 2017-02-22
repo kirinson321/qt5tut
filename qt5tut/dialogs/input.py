5cbf7ca92f67571bd8d9f00aedebc06a53914329
kirinson321 <kirinson321@gmail.com>
2017-02-22 23:20:02 +0100

    text input dialogs

---
+++ qt5tut/dialogs/input.py
@@ -0,0 +1,33 @@
+# simple input dialog box
+
+import sys
+from PyQt5.QtWidgets import QWidget, QPushButton, QLineEdit, QInputDialog, QApplication
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+
+    def initUI(self):
+
+        self.button = QPushButton('Dialog', self)
+        self.button.move(20, 20)
+        self.button.clicked.connect(self.showDialog)
+
+        self.line = QLineEdit(self)
+        self.setWindowTitle('Input dialog')
+        self.show()
+
+
+    def showDialog(self):
+        text, ok = QInputDialog.getText(self, 'Input dialog', 'Enter your name:')
+        if ok:
+            self.line.setText(str(text))
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
